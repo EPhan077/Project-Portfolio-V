b6b6245d1c87470e5a6f2f1c11b489d78c44dced
Eileen Phan <evarina077@fullsail.edu>
2019-02-08 19:38:24 -0800

    added api connect

--- js/index.js
+++ js/index.js
@@ -1,23 +1,86 @@
-const endpoint = `https://www.boredapi.com/api/activity`;
-const button = document.querySelector('.button');
-const form = document.querySelector('#form');
-const activityWrapper = document.querySelector('.activity');
-const typeWrapper = document.querySelector('.type');
+// To set the event to listen to
+const form = document.querySelector('.js-search-form');
+
+// To listen when user hits submit or press enter
+form.addEventListener('submit', (e) => {
+
+
+//  Start of the query string
+const base = `https://www.boredapi.com/api/activity`;
+
+const searchType = `type`;
+const searchParticipants = `participants`;
+const searchCost = `price`;
+const searchAccessibility = `accessibility`;
+
+// Extension of the query string
+const searchTerm = document.querySelector('#searchTerm').value;
+
+// The complete build of the url
+const url = `${base}?${searchType}=${searchTerm}`;
+//const url = `${base}?${searchParticipants}=${searchTerm}`;
+//const url = `${base}?${searchCost}=${searchTerm}`;
+//const url = `${base}?${searchAccessibility}=${searchTerm}`;
 
 
 
-const getActivity = async (event) => {
-  event.preventDefault();
-  const isFree = event.target.children.namedItem('free').checked;
-  console.log(isFree);
-  let Api = isFree ? `${endpoint}?price=0` : endpoint;
-
-  const response = await fetch(Api);
-  const json = await response.json();
-  const {activity, type} = json;
-
-  activityWrapper.innerHTML= activity;
-
+// The get method
+const option = {
+    method: 'GET'
 }
 
-form.addEventListener("submit", getActivity);
+  // Stops the default action
+  e.preventDefault()
+
+    fetch(url)
+
+      //Parse data
+      .then( response => {
+        if (response.ok) {
+            return response.json()
+        } else {
+            throw response
+        }
+    })
+
+     // Do something with data
+      .then( data => {
+
+        console.log(data)
+
+
+
+  // Output results
+  let resultElement = '';
+
+      if(data.activity.length > 1) {
+          resultElement += `
+                          <h2 class="text-center"><span>Results for ${searchTerm}</span></h2>
+                          <section class="js-search-results container-fluid text-center">
+                          `;
+          resultElement += `<a href="#">${data.activity}</a>`;
+          resultElement += `</section>`;
+
+  // To store input via local storage
+  var pastSearch = document.getElementsByTagName('input') [0], search;
+
+  localStorage.setItem('search', searchTerm);
+
+  search = localStorage.getItem('search');
+
+  pastSearch.innerHTML = search;
+
+  console.log(search);
+
+      } else {
+          resultElement += '<p class="no-results">No results</p>';
+      }
+
+      document.querySelector('.js-search-results').innerHTML = resultElement;
+  })
+
+      .catch(err => {
+      console.log(err)
+      })
+
+})
